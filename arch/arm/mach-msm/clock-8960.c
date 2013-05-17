1c74cf882e83c8afd42f683d44bc5f1977aebc81
greg ulok <greg77@gmx.net>
2013-05-17 20:46:29 +0200

    gpu: allow oc to 487.5 & 533.333 Mhz - thx to showp1984 adjusted voltage for the 1782Mhz step for faster bin

--- arch/arm/mach-msm/clock-8960.c
+++ arch/arm/mach-msm/clock-8960.c
@@ -3494,6 +3494,7 @@ static struct clk_freq_tbl clk_tbl_gfx3d_8064[] = {
 	F_GFX3D(325000000, pll15, 1,  3),
 	F_GFX3D(400000000, pll2,  1,  2),
 	F_GFX3D(487500000, pll15, 1,  2),
+	F_GFX3D(533333000, pll2,  2,  3),
 	F_END
 };
 
@@ -3522,7 +3523,7 @@ static struct clk_freq_tbl clk_tbl_gfx3d_8930[] = {
 static unsigned long fmax_gfx3d_8064[MAX_VDD_LEVELS] __initdata = {
 	[VDD_DIG_LOW]     = 128000000,
 	[VDD_DIG_NOMINAL] = 400000000,
-	[VDD_DIG_HIGH]    = 487500000
+	[VDD_DIG_HIGH]    = 533333000
 };
 
 static unsigned long fmax_gfx3d_8930[MAX_VDD_LEVELS] __initdata = {
